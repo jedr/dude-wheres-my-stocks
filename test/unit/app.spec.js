d1523aa5e0b45eabae3e739ff88e9997a66342b0
jedr <andrzej.stencel@gmail.com>
2020-03-16 23:57:04 +0100

    Expect 200 response code on /:ticker route

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,5 +1,10 @@
-describe('App', () => {
-    it('Returns current price for ticker', () => {
-        throw new Error('Not implemented')
-    })
+const app = require("../../src/app")
+const request = require("supertest")
+
+describe("App", () => {
+  it("returns current price for ticker", async () => {
+    await request(app)
+      .get("/AAPL")
+      .expect(200)
+  })
 })
