ad7f8cd943f5047db295b43828bdeb78b655198c
jedr <andrzej.stencel@gmail.com>
2020-03-17 00:15:30 +0100

    Expect /:ticker route to return data from Finnhub
    Using Nock to mock API calls in unit tests.

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,10 +1,27 @@
 const app = require("../../src/app")
+const nock = require("nock")
 const request = require("supertest")
 
 describe("App", () => {
   it("returns current price for ticker", async () => {
+    const apiMock = nock("https://finnhub.io/api/v1")
+      .get("/quote")
+      .query({
+        symbol: "AAPL"
+      })
+      .reply(200, {
+        c: 248.8
+      })
+
     await request(app)
       .get("/AAPL")
       .expect(200)
+      .expect({
+        exchange: "NYSE",
+        ticker: "AAPL",
+        currentPrice: 248.8
+      })
+
+    apiMock.done()
   })
 })
