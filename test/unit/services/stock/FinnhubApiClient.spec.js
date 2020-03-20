bd8f7d67963dabbfa9faa55459778eee998ab158
jedr <andrzej.stencel@gmail.com>
2020-03-20 18:07:41 +0100

    Add unit tests for FinnhubApiClient

---
+++ test/unit/services/stock/FinnhubApiClient.spec.js
@@ -0,0 +1,35 @@
+const expect = require("chai").expect
+const FinnhubApiClient = require("../../../../src/services/stock/FinnhubApiClient")
+const nock = require("nock")
+
+describe("FinnhubApiClient", () => {
+  it("getQuote", async () => {
+    const finnhubApiMock = nock("https://finnhub.io/api/")
+      .get("/v1/quote")
+      .query({
+        symbol: "TIC",
+        token: "api-key"
+      })
+      .reply(200, {
+        o: 5.8,
+        c: 7.5,
+        l: 5.8,
+        h: 7.6,
+        pc: 5.2,
+        t: 123456789
+      })
+    const client = new FinnhubApiClient("api-key")
+
+    const quote = await client.getQuote("TIC")
+
+    expect(quote).to.deep.equal({
+      openPrice: 5.8,
+      currentPrice: 7.5,
+      lowPrice: 5.8,
+      highPrice: 7.6,
+      previousClosePrice: 5.2,
+      timestamp: 123456789
+    })
+
+  })
+})
