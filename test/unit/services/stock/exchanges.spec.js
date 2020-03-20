a2caf69d00996b3e6f62db5ff4a841fc75f14d4d
jedr <andrzej.stencel@gmail.com>
2020-03-20 18:14:48 +0100

    Improve exchanges test readability

--- test/unit/services/stock/exchanges.spec.js
+++ test/unit/services/stock/exchanges.spec.js
@@ -5,8 +5,16 @@ describe("Exchanges", () => {
   it("have well defined structure", () => {
     Object.keys(exchanges).forEach(exchangeKey => {
       const exchange = exchanges[exchangeKey]
-      expect(exchange).to.have.property("name").that.is.a("string").of.length.above(0, `${exchangeKey}.name should not be empty`)
-      expect(exchange).to.have.property("marketIdentifierCode").that.is.a("string").of.length.above(0, `${exchangeKey}.marketIdentifierCode should not be empty`)
+
+      expect(exchange)
+        .to.have.property("name")
+        .that.is.a("string")
+        .of.length.above(0, `${exchangeKey}.name should not be empty`)
+
+      expect(exchange)
+        .to.have.property("marketIdentifierCode")
+        .that.is.a("string")
+        .of.length.above(0, `${exchangeKey}.marketIdentifierCode should not be empty`)
     })
   })
 })
