847f10eae5c2804599bb078d31229f0ec4ab0a13
jedr <andrzej.stencel@gmail.com>
2020-03-23 08:31:49 +0100

    Prevent browser caching

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const v1Router = require("./routes/v1")
+const noCache = require("./noCache.js")
 
 const app = express()
+
+app.use(noCache)
 
 app.use("/v1", v1Router)
 
