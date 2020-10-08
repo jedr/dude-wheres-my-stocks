f6dc2d43a69530ab2e9d297f409159a5cbd6e872
jedr <andrzej.stencel@gmail.com>
2020-10-08 22:54:41 +0200

    Add v2 route for US stocks (#53)

---
+++ src/routes/v2.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const getStockService = require("./../services/stock/getStockService")
+
+const v2Router = express.Router()
+
+v2Router.get("/:ticker", async (req, res, next) => {
+  // Let's pretend the ticker is from NYSE. Finnhub doesn't really care, as long as it is a US stock.
+  exchangeMic = 'XNYS'
+  const ticker = req.params.ticker
+
+  try {
+    const currentPrice = await getStockService().getCurrentStockPrice(exchangeMic, ticker)
+    res.send({
+      ticker,
+      currentPrice
+    })
+  } catch (err) {
+    if (err.status === 429) {
+      return res.sendStatus(err.status)
+    }
+    next(err)
+  }
+})
+
+module.exports = v2Router
--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -3,13 +3,12 @@ const expect = require("chai").expect
 const request = require("supertest")
 
 describe("App", () => {
-  it("/v1/:ticker", async () => {
+  it("/v2/:ticker", async () => {
     await request(app)
-      .get("/v1/CDR")
+      .get("/v2/MSFT")
       .expect(200)
       .then(response => {
-        expect(response.body.exchange).to.equal("XWAR")
-        expect(response.body.ticker).to.equal("CDR")
+        expect(response.body.ticker).to.equal("MSFT")
         expect(response.body.currentPrice).to.be.a("number").that.is.above(0)
       })
   })
