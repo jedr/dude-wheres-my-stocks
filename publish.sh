85300162ded33c09f449e995fd3033e024717751
jedr <andrzej.stencel@gmail.com>
2021-04-11 00:11:01 +0000

    ci: Create tag on successful publish
    But only publish if version was bumped.

--- publish.sh
+++ publish.sh
@@ -6,12 +6,12 @@ declare image_name="jedr/dude-wheres-my-stocks"
 
 declare current_version
 current_version=$(sed 's/.*"version": "\(.*\)".*/\1/;t;d' ./package.json)
-# declare is_published
-# is_published=$(docker manifest inspect "${image_name}:${current_version}" > /dev/null && echo "yes" || echo "no")
-# [[ "${is_published}" == "yes" ]] && exit 0
+declare is_published
+is_published=$(docker manifest inspect "${image_name}:${current_version}" > /dev/null && echo "yes" || echo "no")
+[[ "${is_published}" == "yes" ]] && exit 0
 
 docker buildx build --platform linux/arm/v7,linux/arm64,linux/amd64 --push --tag "${image_name}:${current_version}" .
 
-# declare tag="v${current_version}"
-# git tag --annotate --message="${tag}" "${tag}"
-# git push origin "${tag}"
+declare tag="v${current_version}"
+git tag --annotate --message="${tag}" "${tag}"
+git push origin "${tag}"
